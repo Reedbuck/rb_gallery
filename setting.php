4b62ffc67341bfb5bea197243eaf6f61a71165d3
reedbuck servoplay <reedbuck@mail.ru>
2016-12-11 05:53:26 +0800

    Версия разработки 0.43
    Вывод страниц в отдельный файл

--- setting.php
+++ setting.php
@@ -3,5 +3,6 @@
 require_once RBGL_PLUGIN_DIR . '/activate.php';
 require_once RBGL_PLUGIN_DIR . '/deactivate.php';
 require_once RBGL_PLUGIN_DIR . '/short_code.php';
+require_once RBGL_PLUGIN_DIR . '/homepage.php';
 
 ?>
